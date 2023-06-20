403601e4cdb608022d2d02bca25275240cdc06a5
Nikhil <nikhild.dev.1998@gmail.com>
2023-06-20 08:33:08 +0530

    Intergrated mailchimp

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const request = require("request");
 const bodyParser = require("body-parser");
+const https = require("https");
 
 const app = express();
 
@@ -15,7 +16,40 @@ app.post("/", function (req, res) {
   var lastName = req.body.lname;
   var email = req.body.email;
 
-  console.log(firstName, lastName, email);
+  var data = {
+    members: [
+      {
+        email_address: email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: firstName,
+          LNAME: lastName,
+        },
+      },
+    ],
+  };
+
+  const url = "https://us8.api.mailchimp.com/3.0/lists/ae447a3f8d";
+  const jsonData = JSON.stringify(data);
+  const options = {
+    method: "POST",
+    auth: "nikhil:c87334c8c3d0cdd8c6a8242a75fb52b4-us8",
+  };
+
+  const request = https.request(url, options, function (response) {
+    if (response.statusCode === 200) {
+      res.sendFile(__dirname + "/sucess.html");
+    } else {
+      res.sendFile(__dirname + "/failure.html");
+    }
+
+    response.on("data", function (data) {
+      console.log(JSON.parse(data));
+    });
+  });
+
+  request.write(jsonData);
+  request.end();
 });
 
 app.listen(3000, function () {
