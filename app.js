1be8f34ecf62a3e0efb2140c2a26c1946e48b840
Nikhil <nikhild.dev.1998@gmail.com>
2023-06-20 09:06:01 +0530

    Added Procfile

--- app.js
+++ app.js
@@ -52,6 +52,6 @@ app.post("/", function (req, res) {
   request.end();
 });
 
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
   console.log("Server started at port 3000");
 });
